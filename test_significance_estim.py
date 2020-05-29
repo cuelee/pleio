b2f1701b6073044ef6d10d7cc6c1d174052a4e03
Cue Hyunkyu Lee <cuelee@Cues-MacBook-Pro.local>
2020-05-29 12:46:05 +0900

    2020-05-29:updated README

--- test_significance_estim.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-from numpy.random import uniform
-from framework.significance_estimation import cof_estimation, pvalue_estimation
-
-isof = '/Users/cuelee/Dropbox/github/delpy/isf.isf'
-cof = cof_estimation(isof)
-print(pvalue_estimation(7.5, cof))
