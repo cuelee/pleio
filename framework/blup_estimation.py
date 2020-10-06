4447379b6f83dc8c41b48f6441de830af28285f1
Cue Hyunkyu Lee <cuelee@Cues-MacBook-Pro.local>
2020-10-06 11:22:42 +0900

    added blup_se estimates

--- framework/blup_estimation.py
+++ framework/blup_estimation.py
@@ -6,7 +6,9 @@ def blup_optimization(y, G, R, c):
     pinv_G = np.linalg.pinv(G)
     V = np.linalg.pinv(pinv_R + pinv_G)
     u = V.dot(pinv_R).dot(y)
-    #u_se = np.sqrt(np.diag(V))
-    #return(pd.Series([item for sublist in zip(u, u_se) for item in sublist], index = [item for sublist in zip([a + '_beta' for a in c],[a + '_se' for a in c]) for item in sublist]) )
-    return(pd.Series(u, index = c))
+    if(no.count_nonzero(pinv_G) > 0):
+        u_se = np.sqrt(np.diag(V))
+        return(pd.Series([item for sublist in zip(u, u_se) for item in sublist], index = [item for sublist in zip([a + '_beta' for a in c],[a + '_se' for a in c]) for item in sublist]) )
+    else
+        return(pd.Series([item for sublist in zip(u,[-9]*len(u)) for item in sublist], index = [item for sublist in zip([a + '_beta' for a in c],[a + '_se' for a in c]) for item in sublist]))
 
