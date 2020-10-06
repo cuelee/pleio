f8bf129f8ba13566cdfa2c3b1f565ab4be9f4e1c
Cue Hyunkyu Lee <cuelee@Cues-MacBook-Pro.local>
2020-10-06 10:19:37 +0900

    added blup_se estimates

--- framework/blup_estimation.py
+++ framework/blup_estimation.py
@@ -4,4 +4,7 @@ import pandas as pd
 def blup_optimization(y, G, R, c):
     pinv_R = np.linalg.pinv(R)
     pinv_G = np.linalg.pinv(G)
-    return(pd.Series(np.linalg.pinv(pinv_R + pinv_G).dot(pinv_R).dot(y), index = c))
+    V = np.linalg.pinv(pinv_R + pinv_G)
+    u = V.dot(pinv_R).dot(y)
+    u_se = np.sqrt(np.diag(V))
+    return(pd.Series([item for sublist in zip(u, u_se) for item in sublist], index = [item for sublist in zip([a + '_beta' for a in c],[a + '_se' for a in c]) for item in sublist]) )
